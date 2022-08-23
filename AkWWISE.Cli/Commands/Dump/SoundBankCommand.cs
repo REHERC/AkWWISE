c386b2832417982a3ee7d474e4ee079155a38085
Reherc <amaury.vanoorenberghe@yahoo.com>
2022-08-23 19:33:08 +0200

    Added first version of the cli soundbanks command

---
+++ AkWWISE.Cli/Commands/Dump/SoundBankCommand.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using AkWWISE.Containers.SoundBank;
+using CliFx;
+using CliFx.Attributes;
+using CliFx.Infrastructure;
+
+namespace AkWWISE.Cli.Commands.Dump
+{
+	[Command("soundbank", Description = "Load one or more WWISE .bnk soundbanks")]
+	public class SoundBankCommand : ICommand
+	{
+		[CommandParameter(0, IsRequired = true, Name = "files", Description = "Files to be loaded by the tool")]
+		public IReadOnlyList<FileInfo> Files { get; set; } 
+
+		public ValueTask ExecuteAsync(IConsole console)
+		{
+			AkSoundBankHelper helper = new AkSoundBankHelper();
+
+			List<AkSoundBank> soundbanks = Files
+				.Select(file => helper.LoadFrom(file))
+				.ToList();
+
+			return default;
+		}
+	}
+}
