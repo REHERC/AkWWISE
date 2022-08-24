6e3ca978e6ffa1214a1c082d0b835cebd0afb3ec
Reherc <amaury.vanoorenberghe@yahoo.com>
2022-08-25 00:04:37 +0200

    Added custom platform chunk

---
+++ AkWWISE/Containers/SoundBank/Chunks/AkPLAT.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AkWWISE.IO.Interfaces;
+
+namespace AkWWISE.Containers.SoundBank.Chunks
+{
+	public class AkPLAT : ChunkBase
+	{
+		#region Initialization
+		public override ChunkType Type => ChunkType.PLAT;
+
+		public AkPLAT(AkSoundBank parent) : base(parent)
+		{
+		}
+		#endregion
+
+		#region Properties
+		public string CustomPlatformName => GetField<string>("pCustomPlatformName");
+		#endregion
+
+		public override void Visit(IReader reader)
+		{
+			base.Visit(reader);
+
+			if (AkVersion <= 136)
+			{
+				this.S32STR("pCustomPlatformName", reader);
+			}
+			else
+			{
+				this.STZ("pCustomPlatformName", reader);
+			}
+		}
+	}
+}
