abbc720d4148393c5d41f745cb0c8f581fe00932
Reherc <amaury.vanoorenberghe@yahoo.com>
2022-08-25 00:01:20 +0200

    Added node printer class to create text representation of hierarchies

---
+++ AkWWISE.Cli/Services/NodePrinter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AkWWISE.Core.Nodes;
+
+namespace AkWWISE.Cli.Services
+{
+	public class NodePrinter
+	{
+		public static readonly Type NODE_FIELD = typeof(NodeField<>);
+
+		public string GetString(NodeElement node)
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.AppendLine(node.ToNodeString());
+			Print(sb, node);
+			return sb.ToString();
+		}
+
+		protected void Print(StringBuilder sb, NodeElement node, int depth = 0)
+		{
+			void Write(string text, bool newLine = true)
+			{
+				sb.Append(' ', 2 * depth);
+				if (newLine)
+				{
+					sb.AppendLine(text);
+				}
+				else
+				{
+					sb.Append(text);
+				}
+			}
+
+			foreach (KeyValuePair<string, NodeElement> property in node)
+			{
+				string name = property.Key;
+				NodeElement subNode = property.Value;
+				string value = subNode.ToNodeString();
+
+				Write($"- {name}: {value}");
+
+				Print(sb, subNode, depth + 1);
+			}
+		}
+	}
+}
